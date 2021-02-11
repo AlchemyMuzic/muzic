bbc73d55aeb720017148c371907add7a00fbfaf7
Franco Ortega <francopdx@gmail.com>
2021-02-11 11:21:14 -0800

    add initial folders; initial App architecture; initial Search.jsx

---
+++ src/components/app/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route, 
+  Switch
+} from 'react-router-dom';
+import Search from '../search/Search';
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Search} />
+      </Switch>
+    </Router>
+  );
+}
